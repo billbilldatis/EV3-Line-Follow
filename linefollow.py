2c55d228bf52cbdbb464e6882c0c9f11d5c7b331
billbilldatis <40182400+billbilldatis@users.noreply.github.com>
2021-05-21 20:18:13 +0300

    Update[0]
    Οι κινητηρες παιρνουν power 0-100%

--- linefollow.py
+++ linefollow.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pybricks-micropython
-
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait
@@ -13,21 +12,23 @@ der = 0
 # Initialize the color sensor.
 line_sensor[0] = ColorSensor(Port.S3)
 line_sensor[1] = ColorSensor(Port.S2)
-
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
 # Initialize the drive base.
-robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
-#DRIVE_SPEED = 100
-#kp = 1.2
-#kd = 10
 # Start following the line endlessly.
 def line_follow(DRIVE_SPEED,kp,kd):
-    global error,line_sensor,der,last_error,robot
+    global error,line_sensor,der,last_error,left_motor,right_motor
+    line_sensor[1] = ColorSensor(Port.S2)
+    line_sensor[0] = ColorSensor(Port.S3)
     # Calculate the error from the threshold.
     error = line_sensor[0].reflection() - line_sensor[1].reflection()
     # Calculate the turn rate.
     der = last_error - error
     turn_rate = kp * error + (kd*last_error)
+    speeda = DRIVE_SPEED - turn_rate
+    speedb = DRIVE_SPEED + turn_rate
     # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
+    left_motor.dc(speeda)
+    right_motor.dc(speedb)
     last_error = error
 line_follow(100,0.4,4)
